2907400dbb3cbfb175c0b15a7d1a71459cfa4c14
Jeon <Jeon@DESKTOP-UPD7V7A>
2017-03-24 09:12:11 +0900

    실습과제4-1 제출!

---
+++ JavaPractice/src/org/dimigo/oop/Car.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *	 |_Car
+ * 
+ * 1. 개요:
+ * 2. 작성일 : 2017. 3. 23.
+ * </pre>
+ * 
+ * @author		: Jeon
+ * @version		: 1.0
+ */
+public class Car {
+	private String company;
+	private String model;
+	private String color;
+	private int maxSpeed;
+	private int price;
+	
+	public String getCompany(){
+		return company;
+	}
+	public String getModel(){
+		return model;
+	}
+	public String getColor(){
+		return color;
+	}
+	public int getMaxSpeed(){
+		return maxSpeed;
+	}
+	public int getPrice(){
+		return price;
+	}
+	
+	public void setCompany(String newCompany){
+		company = newCompany;
+	}
+	public void setModel(String newModel){
+		model = newModel;
+	}
+	public void setColor(String newColor){
+		color = newColor;
+	}
+	public void setMaxSpeed(int newmaxSpeed){
+		maxSpeed = newmaxSpeed;
+	}
+	public void setPrice(int newPrice){
+		price = newPrice;
+	}
+	
+	
+	
+	
+	
+}
