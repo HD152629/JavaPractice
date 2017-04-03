9a2906bbf98e0d06290e1c23fd7ec424c63eee34
Jeon <Jeon@DESKTOP-UPD7V7A>
2017-04-03 12:49:27 +0900

    실습과제5 제출!

--- JavaPractice/src/org/dimigo/oop/Question.java
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -27,6 +27,8 @@ public class Question {
 		String[] questions = {"1.가장 좋아하는 가수는?","2.가장 좋아하는 배우는?","3.가장 좋아하는 과목은?"};
 		String[] answers = {"김광석","박보영","공업수학"};
 		
+		StringBuilder result = new StringBuilder("<<결과 출력>>");
+		
 		Scanner scanner = new Scanner(System.in);
 
 		for(int i=0; i<3; i++){
@@ -35,16 +37,13 @@ public class Question {
 			System.out.println(answers[i].equals(answer) ? "정답입니다!" : "틀렸습니다!");
 		}
 		
-		System.out.println("<<결과 출력>>");
-		for(int i=0; i<3; i++){
-			System.out.println(questions[i] + " " + answers[i] + "입니다");
-		}
+		System.out.println(result.append("\n").append(questions[0]).append(" "+answers[0]+"입니다\n").append(questions[1]).append(" "+answers[1]+"입니다\n").append(questions[2]).append(" "+answers[2]+"입니다\n"));
 		
 		
 		
 		
 		
-		
+	
 
 	}
 
