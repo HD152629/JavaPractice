acd3717570a852a0a552b06a1f163ddf6910be2a
Jeon <Jeon@DESKTOP-UPD7V7A>
2017-04-18 10:41:18 +0900

    실습과제8 제출!

---
+++ JavaPractice/src/org/dimigo/oop/FamilyMember.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *	 |_FamilyMember
+ * 
+ * 1. 개요:
+ * 2. 작성일 : 2017. 4. 18.
+ * </pre>
+ * 
+ * @author		: Jeon
+ * @version		: 1.0
+ */
+public class FamilyMember {
+
+	private static int memberCnt;
+	private String memberName;
+	
+	public FamilyMember(){
+		
+	}
+	
+	public FamilyMember(String MemberName){
+		this.memberName = MemberName;
+	}
+
+	/**
+	 * @return the memberName
+	 */
+	public String getMemberName() {
+		return memberName;
+	}
+	
+	public static void printMemberCnt(){
+		System.out.println("가족 총 인원 수 : 4명");
+	}
+	
+	
+}
