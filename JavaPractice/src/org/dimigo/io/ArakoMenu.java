cd4df14e24c1cc8ba8dd0cc56b86b32b3b597441
Jeon <Jeon@DESKTOP-UPD7V7A>
2017-06-16 09:47:02 +0900

    실습과제 17 제출!

--- JavaPractice/src/org/dimigo/io/ArakoMenu.java
+++ JavaPractice/src/org/dimigo/io/ArakoMenu.java
@@ -27,7 +27,7 @@ public class ArakoMenu {
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 				Writer writer = new BufferedWriter(new FileWriter("files/menu.txt"));
 				BufferedReader reader = new BufferedReader(new FileReader("files/menu.txt"))
-				) {
+				) { 
 			
 			String str;
 			while((str =br.readLine())!=null){
